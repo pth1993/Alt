e4f41464a95b5a250f5cb14d7f3b26f4b8542b7c
pth1993 <phamthaihoang.hn@gmail.com>
2016-10-17 13:35:31 +0700

    Add seq

---
+++ seq/lstm.py
@@ -0,0 +1,4 @@
+from keras.models import Sequential
+
+
+model = Sequential()
---
+++ seq/utils.py
@@ -0,0 +1,191 @@
+import codecs
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pickle
+
+
+num_word = 24886
+num_tag = 46
+vector_length = 300
+
+
+def convert_word_to_id(filename1, filename2, word_name):
+    f1 = codecs.open(filename1, 'r', 'utf-8')
+    f2 = codecs.open(filename2, 'w', 'utf-8')
+    data1 = []
+    data2 = []
+    for line in f1:
+        line = line.split()
+        data1 = data1 + line
+        data2.append(line)
+    le = LabelEncoder()
+    le.fit(data1)
+    for line in data2:
+        line = le.transform(line).tolist()
+        f2.write(' '.join(str(x) for x in line) + '\n')
+    f1.close()
+    f2.close()
+    word_dict = list(le.classes_)
+    if word_name == 'word':
+        with open('word_dict.pkl', 'wb') as output:
+            pickle.dump(word_dict, output, pickle.HIGHEST_PROTOCOL)
+    elif word_name == 'tag':
+        with open('tag_dict.pkl', 'wb') as output:
+            pickle.dump(word_dict, output, pickle.HIGHEST_PROTOCOL)
+    return word_dict
+
+
+def cut_data(filename1, filename2, max_len, word_name):
+    f1 = codecs.open(filename1, 'r', 'utf-8')
+    f2 = codecs.open(filename2, 'w', 'utf-8')
+    if word_name == 'word':
+        temp = (u'24886 ' * 5)[0:-1]
+    elif word_name == 'tag':
+        temp = (u'46 ' * 5)[0:-1]
+    for line in f1:
+        line = line.split()
+        num_bulk = len(line)/max_len+1
+        if word_name == 'word':
+            for i in range(max_len-len(line)%max_len):
+                line.append(u'24886')
+        elif word_name == 'tag':
+            for i in range(max_len-len(line)%max_len):
+                line.append(u'46')
+        #print line
+        for i in range(num_bulk):
+            #print line
+            #print line[(i*max_len):((i+1)*max_len)]
+            new_line = ' '.join(line[(i*max_len):((i+1)*max_len)])
+            if new_line != temp:
+                f2.write(new_line+'\n')
+    f1.close()
+    f2.close()
+
+
+def load_data(filename):
+    f = codecs.open(filename, 'r', 'utf-8')
+    data = []
+    for line in f:
+        line = map(int,line.split())
+        data.append(line)
+    return data
+
+
+def convert_to_onehot(filename, data):
+    f = codecs.open(filename, 'w', 'utf-8')
+    for line in data:
+        print line
+        onehot = np.eye(num_word+1)[line]
+
+
+def load_word2vec(filename):
+    f = codecs.open(filename, 'r', 'utf-8')
+    word2vec_list = []
+    f.readline()
+    for line in f:
+        try:
+            word2vec_list.append(line.split()[0])
+        except:
+            pass
+    f.close()
+    return word2vec_list
+
+
+def convert_number_data(filename1, filename2):
+    f1 = codecs.open(filename1, 'r', 'utf-8')
+    f2 = codecs.open(filename2, 'w', 'utf-8')
+    for line in f1:
+        line = line.split()
+        line_new = []
+        for word in line:
+            if any(char.isdigit() for char in word):
+                line_new.append('<number>')
+            else:
+                line_new.append(word)
+        line_new = ' '.join(line_new)
+        f2.write(line_new+'\n')
+    f1.close()
+    f2.close()
+
+
+def create_word_vector_dict(word_dict, filename):
+    vector_list = []
+    index_list = []
+    word_vector_dict = []
+    f = codecs.open(filename, 'r', 'utf-8')
+    for line in f:
+        line = line.split()
+        try:
+            index = word_dict.index(line[0])
+            vector = line[1:vector_length]
+            vector = [float(i) for i in vector]
+            vector_list.append(vector)
+            index_list.append(index)
+        except:
+            pass
+    vector_list = [x for (y, x) in sorted(zip(index_list, vector_list))]
+    for i in range(len(word_dict)):
+        if i in index_list:
+            word_vector_dict.append(vector_list)
+        else:
+            word_vector_dict.append(np.random.normal(loc=0.0, scale=1.0, size=vector_length).tolist())
+    word_vector_dict.append(np.random.normal(loc=0.0, scale=1.0, size=vector_length).tolist())
+    with open('word_vector_dict.pkl', 'wb') as output:
+        pickle.dump(word_vector_dict, output, pickle.HIGHEST_PROTOCOL)
+    return word_vector_dict
+
+
+def split_data(filename_corpus, filename_train, filename_dev, filename_test):
+    f1 = codecs.open(filename_corpus, 'r', 'utf-8')
+    f2 = codecs.open(filename_train, 'w', 'utf-8')
+    f3 = codecs.open(filename_dev, 'w', 'utf-8')
+    f4 = codecs.open(filename_test, 'w', 'utf-8')
+    count = 0
+    for line in f1:
+        count += 1
+        if count <= 14987:
+            f2.write(line)
+        elif count <= (14987+3466):
+            f3.write(line)
+        else:
+            f4.write(line)
+    f1.close()
+    f2.close()
+    f3.close()
+    f4.close()
+
+
+def export_unknown_word(filename_word2vec, filename_unknown_word, word_dict):
+    word2vec_list = load_word2vec(filename_word2vec)
+    temp = list(set(word_dict) - set(word2vec_list))
+    f = codecs.open(filename_unknown_word, 'w', 'utf-8')
+    for item in temp:
+        f.write(item + '\n')
+    f.close()
+
+
+if __name__ == "__main__":
+    print 'Reduce number'
+    convert_number_data('corpus-word.txt', 'corpus-word-reduce-num.txt')
+    print 'Convert word to id'
+    word_dict = convert_word_to_id('corpus-word-reduce-num.txt', 'corpus-word-id.txt', 'word')
+    print 'Convert tag to id'
+    tag_dict = convert_word_to_id('corpus-tag.txt', 'corpus-tag-id.txt', 'tag')
+    print 'Create word vector dict'
+    create_word_vector_dict(word_dict, 'GoogleNews-vectors-negative300.txt')
+    print 'Export unknown word'
+    export_unknown_word('GoogleNews-vectors-negative300.txt', 'unknown_words.txt', word_dict)
+    print 'Split data'
+    split_data('corpus-word-id.txt', 'train-word-id.txt', 'testa-word-id.txt', 'testb-word-id.txt')
+    split_data('corpus-tag-id.txt', 'train-tag-id.txt', 'testa-tag-id.txt', 'testb-tag-id.txt')
+    print 'Padding data'
+    cut_data('train-word-id.txt', 'train-word-id-pad.txt', 5, 'word')
+    cut_data('train-word-id.txt', 'testa-word-id-pad.txt', 5, 'word')
+    cut_data('testb-word-id.txt', 'testb-word-id-pad.txt', 5, 'word')
+    cut_data('train-tag-id.txt', 'train-tag-id-pad.txt', 5, 'tag')
+    cut_data('train-tag-id.txt', 'testa-tag-id-pad.txt', 5, 'tag')
+    cut_data('testb-tag-id.txt', 'testb-tag-id-pad.txt', 5, 'tag')
+    #data = load_data('train-word-id-new.txt')
+    #print np.shape(data)
+    #print 'haha'
+    #data_onehot = convert_to_onehot(data)
