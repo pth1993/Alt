c276abaa83458b1c5e46930a34607b67d1d67f7e
pth1993 <phamthaihoang.hn@gmail.com>
2016-10-28 17:06:07 +0700

    add custom objective

--- seq/ner-jap/lstm_batch.py
+++ seq/ner-jap/lstm_batch.py
@@ -13,6 +13,7 @@ import os
 import argparse
 from keras import backend as K
 from theano import tensor as T
+from theano import shared
 from theano.tensor import basic as tensor
 
 
@@ -54,7 +55,8 @@ def categorical_crossentropy_new(y_true, y_pred):
     output = T.clip(output, _EPSILON, 1.0 - _EPSILON)
     coding_dist = output
     true_dist = target
-    return -tensor.sum(true_dist * tensor.log(coding_dist),
+    bias = shared(np.array([1,1,1,1,100,100]))
+    return -tensor.sum(true_dist * tensor.log(coding_dist) * bias,
                        axis=coding_dist.ndim - 1)
 
 
@@ -85,9 +87,9 @@ with open('word_vector_dict.pkl', 'rb') as input:
 
 print 'Create data to train'
 input_train, output_train = create_data('train-word-id-pad.txt', 'train-tag-id-pad.txt', word_vector_dict)
-#input_test, output_test = create_data('test-word-id-pad.txt', 'test-tag-id-pad.txt', word_vector_dict)
-input_test = input_train
-output_test = output_train
+input_test, output_test = create_data('test-word-id-pad.txt', 'test-tag-id-pad.txt', word_vector_dict)
+#input_test = input_train
+#output_test = output_train
 
 print np.shape(input_train), np.shape(output_train), np.shape(input_test), np.shape(output_test)
 
@@ -112,14 +114,14 @@ history = model.fit(input_train, output_train, batch_size=batch_size, nb_epoch=5
 weights = model.get_weights()
 np.save('model/weight' + '_' + str(num_hidden_node) + '_' + str(dropout), weights)
 answer = model.predict_classes(input_test, batch_size=batch_size)
-#utils.predict_to_file('test-predict-id.txt', 'test-tag-id.txt', answer)
-utils.predict_to_file('test-predict-id.txt', 'train-tag-id.txt', answer)
+utils.predict_to_file('test-predict-id.txt', 'test-tag-id.txt', answer)
+#utils.predict_to_file('test-predict-id.txt', 'train-tag-id.txt', answer)
 with open('le_word.pkl', 'rb') as input:
     le_word = cPickle.load(input)
 with open('le_tag.pkl', 'rb') as input:
     le_tag = cPickle.load(input)
-#utils.convert_to_conll_format('test-predict-id.txt', 'test-tag-id.txt', 'test-word-id.txt', le_word, le_tag, num_tag)
-utils.convert_to_conll_format('test-predict-id.txt', 'train-tag-id.txt', 'train-word-id.txt', le_word, le_tag, num_tag)
+utils.convert_to_conll_format('test-predict-id.txt', 'test-tag-id.txt', 'test-word-id.txt', le_word, le_tag, num_tag)
+#utils.convert_to_conll_format('test-predict-id.txt', 'train-tag-id.txt', 'train-word-id.txt', le_word, le_tag, num_tag)
 input = open('conll_output.txt')
 output = open(os.path.join('evaluate', 'evaluate' + '_' + str(num_hidden_node) + '_' + str(dropout) + '.txt'), 'w')
 subprocess.Popen(shlex.split("perl conlleval.pl"), stdin=input, stdout=output)
