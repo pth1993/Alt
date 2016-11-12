0ea38e008355f190604e541d99dedc781d8b24b6
pth1993 <phamthaihoang.hn@gmail.com>
2016-11-12 10:23:18 +0000

    add ner-vie

--- seq/ner-vie/lstm_batch.py
+++ seq/ner-vie/lstm_batch.py
@@ -52,9 +52,9 @@ elif word_embedding_name == 'glove':
 elif word_embedding_name == 'senna':
     data_dim = 50
 if pos:
-    data_dim += 36
+    data_dim += 37
 if chunk:
-    data_dim += 14
+    data_dim += 15
 num_hidden_node = int(args.num_hidden_node)
 batch_size = int(args.batch_size)
 dropout = float(args.dropout)
