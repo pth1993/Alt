2189965c4156cf4315018e8736d61d6949f4534c
pth1993 <phamthaihoang.hn@gmail.com>
2016-11-22 22:47:23 +0000

    add evaluation

--- seq/ner-vie/batch.py
+++ seq/ner-vie/batch.py
@@ -2,10 +2,11 @@ import subprocess
 
 for i in range(5):
     #word representation
-    subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
-                    'categorical_crossentropy', '0', '0', '0', str(i)])
-    subprocess.call(['python', 'lstm_batch.py', 'random', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
-                    'categorical_crossentropy', '0', '0', '0', str(i)])
+    """if i > 0:
+        subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
+                         'categorical_crossentropy', '0', '0', '0', str(i)])
+        subprocess.call(['python', 'lstm_batch.py', 'random', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
+                         'categorical_crossentropy', '0', '0', '0', str(i)])
     #num_bi-lstm
     subprocess.call(['python', 'lstm_batch.py', 'random', '100', '1', '64', 'none', '0.0', '0.5', '50', 'adam',
                      'categorical_crossentropy', '0', '0', '0', str(i)])
@@ -22,4 +23,10 @@ for i in range(5):
     subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
                      'categorical_crossentropy', '1', '1', '0', str(i)])
     subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '2', '64', 'none', '0.0', '0.5', '50', 'adam',
-                     'categorical_crossentropy', '1', '1', '1', str(i)])
+                     'categorical_crossentropy', '1', '1', '1', str(i)])"""
+    #num_bi-lstm                                                                                                   
+    subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '1', '64', 'none', '0.0', '0.5', '50', 'adam',
+                     'categorical_crossentropy', '0', '0', '0', str(i)])
+    #drop_out                                                                                                      
+    subprocess.call(['python', 'lstm_batch.py', 'word2vec', '100', '2', '64', 'none', '0.0', '0.0', '50', 'adam',
+                     'categorical_crossentropy', '0', '0', '0', str(i)])
